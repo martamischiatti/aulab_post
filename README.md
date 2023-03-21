9f5b212bae326f47648c88c596edec6f8b157dfa
Marta Mischiatti <mischiatti.marta@gmail.com>
2023-03-21 12:21:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+The Aulab Post
+
+Sito di news con le seguenti funzionalità basato su Laravel e Bootstrap
+
+- Possibilità di registrazione e login, implementato con Livewire
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <div class="container-fluid p-5 bg-info text-center text-white">
+        <div class="row justify-content-center">
+            <h1 class="display-1">The Aulab Post</h1>
+        </div>
+    </div>
+</x-layout>
